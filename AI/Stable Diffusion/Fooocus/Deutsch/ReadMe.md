c7f505c2ffa89dbde4ef9a936a6a2ee60b34c0fc
Klingone <KLiNG0NE@users.noreply.github.com>
2024-11-13 06:43:44 +0100

    Add files via upload

---
+++ AI/Stable Diffusion/Fooocus/Deutsch/ReadMe.md
@@ -0,0 +1,12 @@
+# Tipps und Tricks&nbsp;– Fooocus&nbsp;– Deutsch  
+Tipps und Tricks für Computer und Internet.
+---
+Fooocus ist ein Web-UI für Stable Diffusion. Es ist damit möglich, SD ziemlich einfach zu installieren.
+
+## Fooocus Web-UI in Deutsch
+
+Hier sind Infos darüber, wie das WebUI auf die deutsche Sprache umgestellt wird.
+
+Nach der Installation ist das Fooocus-Web-UI nur in englischer Sprache verfügbar. Dies lässt sich recht einfach ändern. Benötigt wird eine Sprachdatei im JSON-Format mit den übersetzten Texten und eine angepasste BAT-Datei, um das Web-UI mit den passenden Parametern zu starten.
+
+Beide Dateien sind in diesem Ordner auffindbar.
---
+++ AI/Stable Diffusion/Fooocus/Deutsch/run_de.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+REM *×**********************************×***************************************
+REM * Datei:  my_preset.bat                                                    *
+REM * Datum:  2024-11-13                                                       *
+REM * Author: KLiNG0NE                                                         *
+REM * Info:                                                                    *
+REM * Diese Batch-Datei startet Fooocus mit vorgegebenen Voreinstellungen.     *
+REM *×**********************************×***************************************
+REM
+TITLE Fooocus - deutsch
+ECHO     ______                                 
+ECHO    / ____/___  ____  ____  _______  _______
+ECHO   / /_  / __ \/ __ \/ __ \/ ___/ / / / ___/
+ECHO  / __/ / /_/ / /_/ / /_/ / /__/ /_/ (__  ) 
+ECHO /_/    \____/\____/\____/\___/\__,_/____/  
+ECHO wird in 'deutscher Sprache' gestartet. - Strg + C zum Beenden
+.\python_embeded\python.exe -s Fooocus\entry_with_update.py --language de
+PAUSE
